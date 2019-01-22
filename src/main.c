155eb461e21878001d12ea4436753ac6d63eefad
iagocq <18238046+iagocq@users.noreply.github.com>
2019-01-22 14:14:16 -0200

    Formatting

--- src/main.c
+++ src/main.c
@@ -25,45 +25,45 @@ SOFTWARE.
 #define CL_TARGET_OPENCL_VERSION 120
 
 #include <CL/cl.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
+
 
 #ifdef _WIN32
-    #include <windows.h>
+#include <windows.h>
 #else
-    #include <sys/time.h>
+#include <sys/time.h>
 #endif
 #define checkError(error) _checkError(__LINE__, error)
 #define plural(number) (number > 1 ? "s" : "")
 
-#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
-#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
+#define ROTLEFT(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
 
-#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
-#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
-#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
-#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
+#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
+#define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
+#define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
 
 const uint32_t k[64] = {
-    0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-    0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-    0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-    0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-    0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-    0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-    0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-    0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
-};
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
 void sha256round(uint8_t *data, uint32_t *state) {
     uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
     for (i = 0, j = 0; i < 16; ++i, j += 4)
         m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-    for ( ; i < 64; ++i)
+    for (; i < 64; ++i)
         m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
     a = state[0];
     b = state[1];
@@ -75,8 +75,8 @@ void sha256round(uint8_t *data, uint32_t *state) {
     h = state[7];
 
     for (i = 0; i < 64; ++i) {
-        t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
-        t2 = EP0(a) + MAJ(a,b,c);
+        t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
+        t2 = EP0(a) + MAJ(a, b, c);
         h = g;
         g = f;
         f = e;
@@ -108,8 +108,10 @@ char *getPlatformName(cl_platform_id platformId) {
     static char platformName[1024];
     size_t size = 0;
     clGetPlatformInfo(platformId, CL_PLATFORM_NAME, 0, NULL, &size);
-    
-    if (size > 1024) size = 1024;
+
+    if (size > 1024)
+        size = 1024;
+
     clGetPlatformInfo(platformId, CL_PLATFORM_NAME, size, platformName, NULL);
 
     return platformName;
@@ -123,7 +125,7 @@ char *readFile(char *filename, size_t *size) {
     *size = (size_t) ftell(f);
     rewind(f);
 
-    char *contents = malloc(*size);
+    char *contents = (char *) malloc(*size);
     fread(contents, 1, *size, f);
 
     return contents;
@@ -133,16 +135,18 @@ char *getDeviceName(cl_device_id deviceId) {
     static char deviceName[1024];
     size_t size = 0;
     clGetDeviceInfo(deviceId, CL_DEVICE_NAME, 0, NULL, &size);
-    
-    if (size > 1024) size = 1024;
+
+    if (size > 1024)
+        size = 1024;
+
     clGetDeviceInfo(deviceId, CL_DEVICE_NAME, size, deviceName, NULL);
 
     return deviceName;
 }
 
-cl_program createProgram(char* source, size_t len, cl_context context, cl_int *error) {
-    size_t lengths[1] = { len };
-    const char *sources[1] = { source };
+cl_program createProgram(char *source, size_t len, cl_context context, cl_int *error) {
+    size_t lengths[1] = {len};
+    const char *sources[1] = {source};
 
     cl_program program = clCreateProgramWithSource(context, 1, sources, lengths, error);
 
@@ -163,7 +167,7 @@ int main(int argc, char *argv[]) {
         printf("Found %d platform%s\n", platformIdCount, plural(platformIdCount));
     }
 
-    cl_platform_id *platformIds = alloca(platformIdCount * sizeof(cl_platform_id));
+    cl_platform_id *platformIds = (cl_platform_id *) alloca(platformIdCount * sizeof(cl_platform_id));
     clGetPlatformIDs(platformIdCount, platformIds, NULL);
 
     for (cl_uint i = 0; i < platformIdCount; i++) {
@@ -183,7 +187,7 @@ int main(int argc, char *argv[]) {
         printf("Found %d device%s on platform %d\n", deviceIdCount, plural(deviceIdCount), 0);
     }
 
-    cl_device_id *deviceIds = alloca(deviceIdCount * sizeof(cl_device_id));
+    cl_device_id *deviceIds = (cl_device_id *) alloca(deviceIdCount * sizeof(cl_device_id));
     clGetDeviceIDs(platformIds[platformId], CL_DEVICE_TYPE_ALL, deviceIdCount, deviceIds, NULL);
 
     for (cl_uint i = 0; i < deviceIdCount; i++) {
@@ -196,16 +200,15 @@ int main(int argc, char *argv[]) {
 
     size_t maxItemSizes[3];
 
-    clGetDeviceInfo(deviceIds[deviceId], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(maxItemSizes), maxItemSizes, NULL);
+    clGetDeviceInfo(deviceIds[deviceId], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(maxItemSizes),
+                    maxItemSizes, NULL);
     nworkers = maxItemSizes[0];
     printf("Max dimensions: [%u, %u, %u]\n", maxItemSizes[0], maxItemSizes[1], maxItemSizes[2]);
     cl_context_properties contextProperties[] = {
-        CL_CONTEXT_PLATFORM,
-        (cl_context_properties) platformIds[platformId],
-        0, 0
-    };
+        CL_CONTEXT_PLATFORM, (cl_context_properties) platformIds[platformId], 0, 0};
 
-    cl_context context = clCreateContext(contextProperties, deviceIdCount, deviceIds, NULL, NULL, &error);
+    cl_context context =
+        clCreateContext(contextProperties, deviceIdCount, deviceIds, NULL, NULL, &error);
     checkError(error);
 
     size_t sourceLen = 0;
@@ -217,9 +220,11 @@ int main(int argc, char *argv[]) {
     error = clBuildProgram(program, deviceIdCount, deviceIds, NULL, NULL, NULL);
     if (error == CL_BUILD_PROGRAM_FAILURE) {
         size_t logSize;
-        clGetProgramBuildInfo(program, deviceIds[deviceId], CL_PROGRAM_BUILD_LOG, 0, NULL, &logSize);
-        char *buildLog = malloc(logSize);
-        clGetProgramBuildInfo(program, deviceIds[deviceId], CL_PROGRAM_BUILD_LOG, logSize, buildLog, NULL);
+        clGetProgramBuildInfo(program, deviceIds[deviceId], CL_PROGRAM_BUILD_LOG, 0, NULL,
+                              &logSize);
+        char *buildLog = (char *) malloc(logSize);
+        clGetProgramBuildInfo(program, deviceIds[deviceId], CL_PROGRAM_BUILD_LOG, logSize, buildLog,
+                              NULL);
         fprintf(stderr, "Build error!\n%s\n", buildLog);
         return EXIT_FAILURE;
     }
@@ -229,15 +234,18 @@ int main(int argc, char *argv[]) {
     checkError(error);
 
     char prehash[65] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    uint32_t state[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
-    sha256round(prehash, state);
+    uint32_t state[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+                         0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+    sha256round((uint8_t *) prehash, state);
 
-    const size_t globalWorkSize[] = { nworkers, nworkers, 10 };
+    const size_t globalWorkSize[] = {nworkers, nworkers, 10};
     uint32_t nitems = globalWorkSize[0] * globalWorkSize[1] * globalWorkSize[2];
 
-    cl_mem prehashBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 64, state, &error);
+    cl_mem prehashBuffer =
+        clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 64, state, &error);
     checkError(error);
-    cl_mem resultBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, nitems * sizeof(uint32_t), NULL, &error);
+    cl_mem resultBuffer =
+        clCreateBuffer(context, CL_MEM_WRITE_ONLY, nitems * sizeof(uint32_t), NULL, &error);
     checkError(error);
 
     cl_command_queue queue = clCreateCommandQueue(context, deviceIds[deviceId], 0, &error);
@@ -265,14 +273,14 @@ int main(int argc, char *argv[]) {
     error = clFinish(queue);
     checkError(error);
 
-    uint32_t *result = malloc(nitems * sizeof(uint32_t));
+    uint32_t *result = (uint32_t *) malloc(nitems * sizeof(uint32_t));
     error = clEnqueueReadBuffer(queue, resultBuffer, CL_TRUE, 0, nitems * 4, result, 0, NULL, NULL);
     checkError(error);
 
     double elapsedTime;
 #ifdef _WIN32
     QueryPerformanceCounter(&t2);
-    elapsedTime = (float)(t2.QuadPart - t1.QuadPart) / (float)frequency.QuadPart;
+    elapsedTime = (float) (t2.QuadPart - t1.QuadPart) / (float) frequency.QuadPart;
 #else
     gettimeofday(&t2, NULL);
     elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
@@ -282,7 +290,7 @@ int main(int argc, char *argv[]) {
 
     printf("Took %.3fs\n", elapsedTime);
     printf("%d Hashes\n", nitems);
-    printf("%.3f H/s\n", (float)nitems/(float)elapsedTime);
+    printf("%.3f H/s\n", (float) nitems / (float) elapsedTime);
     for (int i = 0; i < nitems; i++) {
         if (result[i] == 1) {
             printf("%d\n", i);
