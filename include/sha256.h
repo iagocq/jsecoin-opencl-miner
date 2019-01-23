fdaebb911573ec99eb6e33f8eaa1d4dd7d05e7f0
iagocq <18238046+iagocq@users.noreply.github.com>
2019-01-23 02:40:35 -0200

    Embed CL code in final binary

---
+++ include/sha256.h
@@ -0,0 +1,44 @@
+/*
+MIT License
+
+Copyright (c) 2019 iagocq
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _SHA256_H_
+#define _SHA256_H_
+
+#include <inttypes.h>
+#include <stdio.h>
+
+#define ROTLEFT(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
+
+#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
+#define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
+#define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
+
+void sha256_round(uint8_t *data, uint32_t *state);
+void sha256_init(uint32_t *state);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -26,6 +26,8 @@ SOFTWARE.
 
 #include <CL/cl.h>
 #include <inttypes.h>
+#include <sha256.cl.h>
+#include <sha256.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -34,67 +36,9 @@ SOFTWARE.
 #else
 #include <sys/time.h>
 #endif
+
 #define checkError(error) _checkError(__LINE__, error)
 #define plural(number) (number > 1 ? "s" : "")
-
-#define ROTLEFT(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
-#define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
-
-#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
-#define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
-#define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
-#define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
-
-const uint32_t k[64] = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-
-void sha256round(uint8_t *data, uint32_t *state) {
-    uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
-
-    for (i = 0, j = 0; i < 16; ++i, j += 4)
-        m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-    for (; i < 64; ++i)
-        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
-    a = state[0];
-    b = state[1];
-    c = state[2];
-    d = state[3];
-    e = state[4];
-    f = state[5];
-    g = state[6];
-    h = state[7];
-
-    for (i = 0; i < 64; ++i) {
-        t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
-        t2 = EP0(a) + MAJ(a, b, c);
-        h = g;
-        g = f;
-        f = e;
-        e = d + t1;
-        d = c;
-        c = b;
-        b = a;
-        a = t1 + t2;
-    }
-
-    state[0] += a;
-    state[1] += b;
-    state[2] += c;
-    state[3] += d;
-    state[4] += e;
-    state[5] += f;
-    state[6] += g;
-    state[7] += h;
-}
 
 void _checkError(int line, cl_int error) {
     if (error != CL_SUCCESS) {
@@ -114,20 +58,6 @@ char *getPlatformName(cl_platform_id platformId) {
     clGetPlatformInfo(platformId, CL_PLATFORM_NAME, size, platformName, NULL);
 
     return platformName;
-}
-
-char *readFile(char *filename, size_t *size) {
-    FILE *f = fopen(filename, "r");
-
-    fseek(f, 0L, SEEK_END);
-
-    *size = (size_t) ftell(f);
-    rewind(f);
-
-    char *contents = (char *) malloc(*size);
-    fread(contents, 1, *size, f);
-
-    return contents;
 }
 
 char *getDeviceName(cl_device_id deviceId) {
@@ -213,7 +143,7 @@ int main(int argc, char *argv[]) {
     checkError(error);
 
     size_t sourceLen = 0;
-    char *source = readFile("sha256.cl", &sourceLen);
+    char *source = sha256CLSource;
 
     cl_program program = createProgram(source, sourceLen, context, &error);
     checkError(error);
@@ -234,17 +164,18 @@ int main(int argc, char *argv[]) {
     cl_kernel kernel = clCreateKernel(program, "sha256", &error);
     checkError(error);
 
-    char prehash[65] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    uint32_t state[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-                         0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
-    sha256round((uint8_t *) prehash, state);
-
     size_t globalWorkSize[3];
 
     for (int i = 0; i < 3; i++) {
         printf("Insert the value for dimension %d of the work size: ", i + 1);
         scanf("%u", &globalWorkSize[i]);
     }
+
+    char prehash[65] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    uint32_t state[8];
+
+    sha256_init(state);
+    sha256_round((uint8_t *) prehash, state);
 
     uint32_t nitems = globalWorkSize[0] * globalWorkSize[1] * globalWorkSize[2];
 
@@ -277,6 +208,7 @@ int main(int argc, char *argv[]) {
 
     error = clEnqueueNDRangeKernel(queue, kernel, 3, NULL, globalWorkSize, NULL, 0, NULL, NULL);
     checkError(error);
+
     error = clFinish(queue);
     checkError(error);
 
@@ -298,13 +230,13 @@ int main(int argc, char *argv[]) {
     printf("Took %.3fs\n", elapsedTime);
     printf("%d Hashes\n", nitems);
     printf("%.3f H/s\n", (double) nitems / elapsedTime);
-/*
+
     for (int i = 0; i < nitems; i++) {
         if (result[i] == 1) {
             printf("%d\n", i);
         }
     }
-*/
+
     clReleaseCommandQueue(queue);
 
     clReleaseMemObject(prehashBuffer);
